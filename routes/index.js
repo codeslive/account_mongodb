ba23a8a7e33d0d06fd64db5c9da946ba2c2b0068
codesLive <2783779285@qq.com>
2023-04-07 18:00:21 +0800

    获取表单数据
    Signed-off-by: codesLive <2783779285@qq.com>

--- routes/index.js
+++ routes/index.js
@@ -11,4 +11,10 @@ router.get('/account/create', function(req, res, next) {
   res.render('create');
 });
 
+//新增记录
+router.post('/account', function(req, res, next) {
+  console.log(req.body);
+  res.send('添加记录');
+});
+
 module.exports = router;
