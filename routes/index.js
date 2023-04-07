47729d8442025923a130b98680ea5c72e8439d14
codesLive <2783779285@qq.com>
2023-04-07 18:21:21 +0800

    保存账单信息
    Signed-off-by: codesLive <2783779285@qq.com>

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,13 @@
 var express = require('express');
 var router = express.Router();
-
+//导入 lowdb
+const low = require('lowdb')
+const FileSync = require('lowdb/adapters/FileSync')
+const adapter = new FileSync(__dirname + '/../data/db.json');
+//获取 db 对象
+const db = low(adapter);
+//导入 shortid
+const shortid = require('shortid');
 //记账本列表
 router.get('/account', function(req, res, next) {
   res.render('list');
@@ -13,8 +20,11 @@ router.get('/account/create', function(req, res, next) {
 
 //新增记录
 router.post('/account', function(req, res, next) {
-  console.log(req.body);
-  res.send('添加记录');
+   //生成 id
+   let id = shortid.generate();
+   //写入文件
+   db.get('accounts').unshift({id:id, ...req.body}).write();
+    res.send('添加记录');
 });
 
 module.exports = router;
