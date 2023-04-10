40774d22db4218d8c02aa8cdb6e2986ab10a37c7
codesLive <2783779285@qq.com>
2023-04-10 14:09:12 +0800

    npm i jsonwebtoken --save
    Signed-off-by: codesLive <2783779285@qq.com>

--- app.js
+++ app.js
@@ -1,33 +1,21 @@
-const createError = require('http-errors');
-const express = require('express');
-const path = require('path');
-const cookieParser = require('cookie-parser');
-const logger = require('morgan');
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
 
-const indexRouter = require('./routes/web/index');
+var indexRouter = require('./routes/web/index');
+const authRouter = require('./routes/web/auth');
+const authApiRouter = require('./routes/api/auth');
+//导入 account 接口路由文件
 const accountRouter = require('./routes/api/account');
-const authRouther = require('./routes/web/auth');
-const app = express();
-
 //导入 express-session 
 const session = require("express-session");
 const MongoStore = require('connect-mongo');
 //导入配置项
 const {DBHOST, DBPORT, DBNAME} = require('./config/config');
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
 
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.use('/', indexRouter);
-app.use('/', authRouther);
-app.use('/api', accountRouter);
-
+var app = express();
 //设置 session 的中间件
 app.use(session({
   name: 'sid',   //设置cookie的name，默认值是：connect.sid
@@ -43,15 +31,29 @@ app.use(session({
   },
 }))
 
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+app.use('/', authRouter);
+app.use('/api', accountRouter);
+app.use('/api', authApiRouter);
+
 // catch 404 and forward to error handler
-app.use(function (req, res, next) {
-  next(createError(404));
+app.use(function(req, res, next) {
+  //响应 404 
+  res.render('404');
 });
 
-
-
 // error handler
-app.use(function (err, req, res, next) {
+app.use(function(err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
